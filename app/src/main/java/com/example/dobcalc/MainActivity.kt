0103a594cf436aa0fe2f85e6d67cbdba4bcc36b1
SOMIL GARAK <sgarak@deakin.edu.au>
2021-11-02 12:34:48 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/dobcalc/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.dobcalc
+
+import android.app.DatePickerDialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnDatePicker : Button = findViewById(R.id.btnDatePicker)
+
+        btnDatePicker.setOnClickListener{
+            clickDatePiker()
+        }
+    }
+    fun clickDatePiker()
+    {
+        val myCalendar = Calendar.getInstance()
+        val year = myCalendar.get(Calendar.YEAR)
+        val month = myCalendar.get(Calendar.MONTH)
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+
+        //DatePickerDialog(this,
+        //DatePickerDialog.OnDateSetListener{
+        //    view, year, month, dayOfMonth ->
+        //    Toast.makeText(this, "Date picker works", Toast.LENGTH_SHORT).show()
+        //}, year, month, day).show()
+        DatePickerDialog(this,
+        { view, year, month, dayOfMonth ->
+            Toast.makeText(this, "Date picker works", Toast.LENGTH_SHORT).show()
+        }, year, month, day).show()
+
+    }
+}
